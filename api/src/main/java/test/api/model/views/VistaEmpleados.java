1e1e86badad1eb11e295c57b815b4d86a3ebe16d
Josu <delaflor.josu@uni.eus>
2025-02-10 23:22:07 +0100

    Update VistaEmpleados.java Change Persistence Imports

--- api/src/main/java/test/api/model/views/VistaEmpleados.java
+++ api/src/main/java/test/api/model/views/VistaEmpleados.java
@@ -1,6 +1,9 @@
 package test.api.model.views;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 
 @Entity
 @Table(name = "vista_empleados")
